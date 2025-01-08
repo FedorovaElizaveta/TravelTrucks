d02a2ad7636e2498a98c3e9579769b73a2830939
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2025-01-08 23:51:21 +0200

    Add "pages" folder and move HomePage to it

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,26 @@ import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Layout from "./components/Layout/Layout.tsx";
 
-const HomePage = lazy(() => import("./components/HomePage/HomePage.tsx"));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.tsx"));
 // const CampersPage = lazy(
-//   () => import("./components/CampersPage/CampersPage.tsx")
+//   () => import("./pages/CampersPage/CampersPage.tsx")
 // );
 // const CampersDetails = lazy(
-//   () => import("./components/CampersDetails/CampersDetails.tsx")
+//   () => import("./pages/CampersDetails/CampersDetails.tsx")
+// );
+// const NotFoundPage = lazy(
+//   () => import("./pages/NotFoundPage/NotFoundPage.tsx")
 // );
 
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      {" "}
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="/campers" element={<CampersPage />} />
-        <Route path="/campers/:id" element={<CampersDetails />} /> */}
+          {/* <Route path="/campers" element={<CampersPage />} /> */}
+          {/* <Route path="/campers/:id" element={<CampersDetails />} /> */}
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
         </Route>
       </Routes>
     </Suspense>
